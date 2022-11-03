c65d13b6f3900026e03f5ab71fb7263294b1672f
nicolay korobeinikov <kolyan125125@yandex.ru>
2022-11-04 02:22:43 +0300

    simple test-test-set lock added

---
+++ src/main/java/ru/ricnorr/numa/locks/mcs/TestTestAndSetLock.java
@@ -0,0 +1,49 @@
+package ru.ricnorr.numa.locks.mcs;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class TestTestAndSetLock implements Lock {
+
+    private AtomicBoolean flag = new AtomicBoolean(false);
+
+    @Override
+    public void lock() {
+        while (true) {
+            if (!flag.get() && flag.compareAndSet(false, true)) {
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void unlock() {
+        flag.set(false);
+    }
+
+    @Override
+    public void lockInterruptibly() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean tryLock() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean tryLock(long l, @NotNull TimeUnit timeUnit) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @NotNull
+    @Override
+    public Condition newCondition() {
+        throw new RuntimeException("Not implemented");
+    }
+}
+
