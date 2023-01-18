38588f54d97c3a4903fc327400be876ed8770606
nicolay korobeinikov <kolyan125125@yandex.ru>
2023-01-19 02:10:24 +0300

    find closest numa cluster

--- src/main/java/ru/ricnorr/numa/locks/CnaLockSpec.java
+++ src/main/java/ru/ricnorr/numa/locks/CnaLockSpec.java
@@ -6,20 +6,25 @@ import ru.ricnorr.benchmarks.LockType;
 
 public class CnaLockSpec {
     public boolean useJavaAtomics = true;
-    public static String USE_JAVA_ATOMICS = "useJavaAtomics";
+    public static String USE_JAVA_ATOMICS = "javatom";
 
     public boolean useParkingOnSpin = true;
-    public static String USE_PARKING_ON_SPIN = "useParkingOnSpin";
+    public static String USE_PARKING_ON_SPIN = "park";
 
     public long spinThreshold = 512;
 
-    public static String SPIN_THRESHOLD = "spinThreshold";
+    public static String SPIN_THRESHOLD = "thres";
+
+    public boolean kunpengNuma = false;
+
+    public static String KUNPENG_NUMA_DIST = "numa";
 
     public CnaLockSpec(String params) {
         var obj = (JSONObject) JSONValue.parse(params);
         this.useJavaAtomics = (Boolean) obj.get(USE_JAVA_ATOMICS);
         this.useParkingOnSpin = (Boolean) obj.get(USE_PARKING_ON_SPIN);
         this.spinThreshold = (Long) obj.get(SPIN_THRESHOLD);
+        this.kunpengNuma = (Boolean) obj.get(KUNPENG_NUMA_DIST);
     }
 
     @Override
