a20a17e0e39f48143082bc400b4cd67772ed8ca9
Nikolai Korobeinikov <ricnorr@yandex-team.ru>
2023-03-11 00:50:17 +0300

    Build fatjar

---
+++ src/main/java/ru/ricnorr/numa/locks/hmcs/HMCSCclNoPad.java
@@ -0,0 +1,17 @@
+package ru.ricnorr.numa.locks.hmcs.nopad;
+
+import ru.ricnorr.numa.locks.Utils;
+
+/**
+ * HMCS, иерархия только на ccl'ях, то есть поток берет лок на своей ccl, затем глобальный лок.
+ */
+public class HMCSCclNoPad extends AbstractHMCS<HMCSQNodeNoPad> {
+
+    public HMCSCclNoPad() {
+        super(HMCSQNodeNoPad::new, Utils::getKunpengCCLId, Utils.CCL_CNT);
+        var root = new HNode(null, new HMCSQNodeNoPad());
+        for (int i = 0; i < Utils.CCL_CNT; i++) {
+            leafs[i] = new HNode(root, null);
+        }
+    }
+}
---
+++ src/main/java/ru/ricnorr/numa/locks/hmcs/HMCSNumaNoPad.java
@@ -0,0 +1,16 @@
+package ru.ricnorr.numa.locks.hmcs.nopad;
+
+
+import ru.ricnorr.numa.locks.Utils;
+
+public class HMCSNumaNoPad extends AbstractHMCS<HMCSQNodeNoPad> {
+
+
+    public HMCSNumaNoPad() {
+        super(HMCSQNodeNoPad::new, Utils::getNumaNodeId, Utils.NUMA_NODES_CNT);
+        var root = new HNode(null, new HMCSQNodeNoPad());
+        for (int i = 0; i < Utils.NUMA_NODES_CNT; i++) {
+            leafs[i] = new HNode(root, new HMCSQNodeNoPad());
+        }
+    }
+}
