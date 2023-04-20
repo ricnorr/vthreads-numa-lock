2326eaf4951d155a1fff690fc3a11a6c680d3d2d
Nikolai Korobeinikov <ricnorr@yandex-team.ru>
2023-04-20 20:41:54 +0300

    res

--- run_bench.sh
+++ run_bench.sh
@@ -2,11 +2,10 @@
 #!/bin/bash
 DIR=${CONDA_PREFIX:-$HOME/miniconda3}
 CONDA_PROFILE=$DIR/etc/profile.d/conda.sh
-
 source "$CONDA_PROFILE"
 conda activate pip3
 sdk use gradle 7.6
-sdk use java 19.0.2-open
+sdk use java custom-19
 gradle run
 cd scripts
 python3 picture_creator
--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -5,3 +5,24 @@ sdk install gradle 7.6
 sdk install java 19.0.2-open
 sdk default gradle 7.6
 sdk default java 19.0.2-open
+cd ~
+if [[ "$1" == ssh ]]; then
+git clone git@github.com:ricnorr/jdk19.git
+else
+git clone https://github.com/ricnorr/jdk19.git
+fi
+cd ~/jdk19
+git checkout enhancement
+bash configure
+make images
+if [ `uname -s` == "Darwin"  ] && [ `uname -m` == "x86_64"  ]; then
+  INST="macosx-x86_64-server-release"
+elif [ `uname -s` == "Darwin"  ] && [ `uname -m` == "arm64"  ]; then
+  INST="macosx-aarch64-server-release"
+elif [ `uname -s` == "Linux"  ] && [ `uname -m` == "aarch64"  ]; then
+  INST="linux-aarch64-server-server-release"
+elif [ `uname -s` == "Linux"  ] && [ `uname -m` == "x86_64"  ]; then
+  INST="linux-x86_64-server-server-release"
+fi
+sdk install java custom-19 ~/jdk19/build/{$INST}/jdk/bin
+
