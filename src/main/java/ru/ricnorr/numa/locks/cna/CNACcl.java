aea7a729068f5ccf746017f60310228e7fecd936
Nikolai Korobeinikov <ricnorr@yandex-team.ru>
2023-05-21 12:51:00 +0300

    solution

--- src/main/java/ru/ricnorr/numa/locks/cna/CNACcl.java
+++
@@ -1,9 +0,0 @@
-package ru.ricnorr.numa.locks.cna;
-
-import ru.ricnorr.numa.locks.Utils;
-
-public class CNACcl extends AbstractCNA<CNANode> {
-    public CNACcl() {
-        super(Utils::getKunpengCCLId, CNANode::new);
-    }
-}
--- src/main/java/ru/ricnorr/numa/locks/hclh/HCLHCclNoPad.java
+++
@@ -1,9 +0,0 @@
-package ru.ricnorr.numa.locks.hclh;
-
-import ru.ricnorr.numa.locks.Utils;
-
-public class HCLHCclNoPad extends AbstractHCLHLock<HCLHNodeNoPad> {
-    public HCLHCclNoPad() {
-        super(HCLHNodeNoPad::new, Utils::getKunpengCCLId);
-    }
-}
