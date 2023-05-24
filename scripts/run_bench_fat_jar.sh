ffe02b5dd080a4a52539c3bf6f1978ef28c7e48c
Nikolai Korobeinikov <ricnorr@yandex-team.ru>
2023-05-24 08:23:31 +0300

    fix run_bench_fat_jar.sh

--- scripts/run_bench_fat_jar.sh
+++ scripts/run_bench_fat_jar.sh
@@ -9,7 +9,7 @@ sdk use java 19.0.2-open
 
 cp bench/settings/settings.json fatjar/settings/settings.json
 cd fatjar
-java -XX:+UseParallelGC -XX:+UseNUMA -XX:-RestrictContended --add-opens java.base/java.lang=ALL-UNNAMED  --enable-preview -Djdk.trackAllThreads=true -Djdk.tracePinnedThreads=true  -Djna.library.path=../libs/ -jar bench.jar
+java -XX:+UseParallelGC -XX:+UseNUMA -XX:-RestrictContended --add-opens java.base/java.lang=ALL-UNNAMED  --enable-preview -Djdk.trackAllThreads=true -Djdk.tracePinnedThreads=true  -Djna.library.path=../bench/libs/ -jar bench.jar
 cp ./results/benchmark_results.csv ../results/benchmark_results.csv
 rm -rf ./results/benchmark_results.csv
 cd ../scripts/
